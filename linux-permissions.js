ec4a76fd5d4a91bf4ee83d41b875de262bac4bf3
veselina99 <92113988+veselina99@users.noreply.github.com>
2021-11-03 13:20:37 +0200

    Update linux-permissions.js

--- linux-permissions.js
+++ linux-permissions.js
@@ -1,20 +1,25 @@
 const input = require('fs') // required to work with the filesystem
     .readFileSync(0) // read a buffer from stdin file descriptor
     .toString('utf8');
-
-let permissions = input.split('')
-let binaryNumber = []
-for(let i = 0; i < permissions.length; i++)
+function decimalInteger(letters)
 {
-    if(permissions[i] === 'x' || permissions[i] === 'w' || permissions[i] === 'r') binaryNumber[i] = 1
-    else if(permissions[i] === '-') binaryNumber[i] = 0
-}
-let decimalInteger = 0
-let counter = 0
-for(let i = binaryNumber.length - 1; i >= 0; i--)
-{
-    decimalInteger += binaryNumber[i]*Math.pow(2,counter)
-    counter++
+    let permissions = letters.split('')
+    let binaryNumber = []
+    for(let i = 0; i < permissions.length; i++)
+    {
+        if(permissions[i] === 'x' || permissions[i] === 'w' || permissions[i] === 'r') binaryNumber[i] = 1
+        else if(permissions[i] === '-') binaryNumber[i] = 0
+    }
+    let decimalInteger = 0
+    let counter = 0
+    for(let i = binaryNumber.length - 1; i >= 0; i--)
+    {
+        decimalInteger += binaryNumber[i]*Math.pow(2,counter)
+        counter++
+    }
+    return decimalInteger
+
 }
 
-console.log(decimalInteger)
+console.log(decimalInteger(input))
+
